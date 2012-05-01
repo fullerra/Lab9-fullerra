d8f565e098d9a4fd0a241722abfe5f6403573097
Ryan Fuller <fullerra@rose-hulman.edu>
2012-05-01 18:41:03 -0400

    finished all tasks and pass all tests, added who.txt file

--- ChatBot-TeamMe/chatbot.rb
+++ ChatBot-TeamMe/chatbot.rb
@@ -6,4 +6,8 @@ games = ["Game1", "Game2", "Game3", "Game4", "Global Thermonuclear War"]
 puts "Games I can play: #{games}"
 puts "Choose game: 1-5"
 number = gets.chomp
-puts "I refuse to play that game!"
+if games[number.to_i - 1]=="Global Thermonuclear War"
+  puts "BOOM!"
+else
+  puts "I refuse to play that game!"
+end
